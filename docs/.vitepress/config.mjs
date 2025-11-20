c4d2b831126dfca716d3f688a031d0ee22d59267
Dano Day <danoday@Foxmail.com>
2025-11-20 22:47:17 +0800

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
     title: 'Pressidian',
+    base: '/pressidian/',
     description: '一个使用Obsidian笔记仓库构建的VitePress静态站点',
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
